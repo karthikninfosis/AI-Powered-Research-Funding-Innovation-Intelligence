b163104e8381b31db9f844cfc5b2c62595682797
Karthik7411 <karthik648464@gmail.com>
2026-09-13 14:24:01 +0530

    integrtaion

---
+++ frontend/landingPage/js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const exploreButton = document.getElementById("exploreIntelligence");
+
+    if (exploreButton) {
+        exploreButton.addEventListener("click", function () {
+            window.location.href = "funding.html";
+        });
+    }
+
+    const startButton = document.getElementById("startExploring");
+
+    if (startButton) {
+        startButton.addEventListener("click", function () {
+            window.location.href = "funding.html";
+        });
+    }
+
+});
